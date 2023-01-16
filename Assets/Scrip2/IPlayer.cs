a184484852e1474e62870ae0610fb59e9a507b0c
a154254929 <664251332@qq.com>
2023-01-16 17:40:42 +0800

    dotween插件，模型等。

--- Assets/Scrip2/IPlayer.cs
+++ Assets/Scrip2/IPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -25,12 +26,15 @@ namespace GameJamDemo
         private MoveDirection m_direction;
         private Vector3Int m_position;
         private GameObject m_obj;
+        private Animator m_animator;
+
 
         protected bool IsSelf;
         public BasePlayer(Vector3Int initPos, MoveDirection initDir, GameObject obj)
         {
             m_position = initPos;
             m_obj = GameObject.Instantiate(obj);
+            m_animator = m_obj.GetComponent<Animator>();
             m_direction = initDir;
             SetPos(m_position);
         }
@@ -39,13 +43,32 @@ namespace GameJamDemo
         {
             m_direction = direction;
             //更新模型的方向
-
+            switch (direction)
+            {
+                case MoveDirection.Forward:
+                    m_obj.transform.eulerAngles = new Vector3(0, 0, 0);
+                    break;
+                case MoveDirection.BackWard:
+                    m_obj.transform.eulerAngles = new Vector3(0, 180, 0);
+                    break;
+                case MoveDirection.Left:
+                    m_obj.transform.eulerAngles = new Vector3(0, 270, 0);
+                    break;
+                case MoveDirection.Right:
+                    m_obj.transform.eulerAngles = new Vector3(0, 90, 0);
+                    break;
+            }
         }
 
         public bool Move()
         {
             bool gameOver = false;
             Vector3Int result = mapManager.TryMove(m_position, m_direction, out gameOver);
+            if (m_position != result)
+            {
+                m_animator.SetTrigger("Jump");
+            }
+
             SetPos(result);
             //Debug.LogFormat("curPos {0}, dir {1}, result {2}", m_position, m_direction, result);
             return gameOver;
@@ -57,11 +80,25 @@ namespace GameJamDemo
         /// <param name="pos"></param>
         public void SetPos(Vector3Int pos)
         {
-            m_position = pos;
+            m_position = pos; 
             mapManager.HideUpLayer(pos.z);
             var block = mapManager.GetBlock(pos);
             var blockPos = block.GetObjTransPosition();
-            m_obj.transform.position = blockPos + gameConfig.PlayerPosOffset;
+
+            var targetTransPos = blockPos + gameConfig.PlayerPosOffset;
+            if (Vector3.Distance(m_obj.transform.position, targetTransPos) > 0.2f)
+            {
+                DOTween.To(() => m_obj.transform.position, r => m_obj.transform.position = r, targetTransPos, 0.4f);
+            }
+            
+            //m_obj.transform.position = targetTransPos;
+        }
+
+        public Vector3 GetTargetTransPos(Vector3Int pos)
+        {
+            var block = mapManager.GetBlock(pos);
+            var blockPos = block.GetObjTransPosition();
+            return blockPos + gameConfig.PlayerPosOffset;
         }
 
         /// <summary>
@@ -80,6 +117,7 @@ namespace GameJamDemo
         public void SetBomb()
         {
             bombManager.AddBomb(m_position);
+            mapManager.ChangeExplodeColor(m_position);
             //Debug.LogFormat("释放炸弹{0}", m_position);
         }
 
